9cd6420ab83ea3e0d1c45ba1ed21ad5232b8014d
Samuel de Leon <sapdeleon@msn.com>
2022-03-26 14:58:14 -0400

    Remove .vs and learn import/export/modules in ES6

--- src/intro/App.jsx
+++ src/intro/App.jsx
@@ -1,10 +1,23 @@
 import React from "react"
 import Greeting from "./Greeting";
+import { pi, doublePi, triplePi, add, multiply, subtract, divide } from "./math"
 
 const App = () => {
     return (
         <div>
             <Greeting />
+            <ul>
+                <li>{pi}</li>
+                <li>{doublePi()}</li>
+                <li>{triplePi()}</li>
+                <li><p>Math Operations</p></li>
+                <ul>
+                    <li>{add(1, 2)}</li>
+                    <li>{multiply(2, 3)}</li>
+                    <li>{subtract(7, 2)}</li>
+                    <li>{divide(5, 2)}</li>
+                </ul>
+            </ul>
         </div>
     );
 }
---
+++ src/intro/math.js
@@ -0,0 +1,15 @@
+const pi = 3.1415962
+
+const doublePi = () => pi * 2
+
+const triplePi = () => pi * 3
+
+const add = (num1, num2) => num1 + num2
+
+const multiply = (num1, num2) => num1 * num2
+
+const subtract = (num1, num2) => num1 - num2
+
+const divide = (num1, num2) => num1 / num2
+
+export { pi, doublePi, triplePi, add, multiply, subtract, divide }
