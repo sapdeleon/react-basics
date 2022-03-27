2e55e51b813e01214eef9e73c1d23c96eaafa12a
Samuel de Leon <sapdeleon@msn.com>
2022-03-26 22:34:23 -0400

    Using props in books part-2

--- src/book/App.jsx
+++ src/book/App.jsx
@@ -3,33 +3,45 @@ import React from "react"
 const App = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book img={booklist[0].img} title={booklist[0].title} author={booklist[0].author} />
+      <Book img={booklist[1].img} title={booklist[1].title} author={booklist[1].author} />
+      <Book img={booklist[2].img} title={booklist[2].title} author={booklist[2].author} />
     </section>
   )
 }
 
-const Book = () => {
+const booklist = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/5110qJqtLiL._SX403_BO1,204,203,200_.jpg",
+    alt: "Programming",
+    title: "The Little Book Of C# Programming",
+    author: "Huw Collingbourne"
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/517m8+OZoTS._SX258_BO1,204,203,200_.jpg",
+    alt: "Python-Programming",
+    title: "Python Programming for Beginners",
+    author: "AMZ Publishing"
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41g3C9Ad-zL._SX404_BO1,204,203,200_.jpg",
+    alt: "Python-Programming",
+    title: "Learn Python Programming",
+    author: "Fabrizio Romano & Heinrich Kruger"
+  }
+];
+
+const Book = (props) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt={props.alt} />
+      <h1>{props.title}</h1>
+      <h4>by {props.author}</h4>
     </article>
   )
 };
-
-const Image = () => (
-  <img src="https://m.media-amazon.com/images/I/41d5DXqG+dL._AC_UL320_.jpg" alt="Sql-Server" />
-);
-
-const Title = () => (
-  <h1>Sql Svr Developer Edit 2008 DVD</h1>
-);
-
-const Author = () => (
-  <h4>by Microsoft</h4>
-);
 
 export default App;
