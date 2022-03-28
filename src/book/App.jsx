a45e74e0a2b521a89e5d74af7a44e8940e50a5bf
Samuel de Leon <sapdeleon@msn.com>
2022-03-27 22:12:40 -0400

    Destructuring props and adding children.

--- src/book/App.jsx
+++ src/book/App.jsx
@@ -4,11 +4,28 @@ const App = () => {
   return (
     <section className="booklist">
       <Book img={booklist[0].img} title={booklist[0].title} author={booklist[0].author} />
-      <Book img={booklist[1].img} title={booklist[1].title} author={booklist[1].author} />
+      <Book img={booklist[1].img} title={booklist[1].title} author={booklist[1].author}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.</p>
+      </Book>
       <Book img={booklist[2].img} title={booklist[2].title} author={booklist[2].author} />
     </section>
   )
 }
+
+const Book = (props) => {
+  const { img, alt, title, author, children } = props
+  return (
+    <article className="book">
+      <img src={img} alt={alt} />
+      <h1>{title}</h1>
+      <h4>by {author}</h4>
+      {children}
+    </article>
+  )
+};
 
 const booklist = [
   {
@@ -33,15 +50,5 @@ const booklist = [
     author: "Fabrizio Romano & Heinrich Kruger"
   }
 ];
-
-const Book = (props) => {
-  return (
-    <article className="book">
-      <img src={props.img} alt={props.alt} />
-      <h1>{props.title}</h1>
-      <h4>by {props.author}</h4>
-    </article>
-  )
-};
 
 export default App;
