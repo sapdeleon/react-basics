1e71e24081eb3b1e622f849fd34bb8b00c697a61
Samuel de Leon <sapdeleon@msn.com>
2022-03-27 22:32:33 -0400

    Using map to simplify the booklist.

--- src/book/App.jsx
+++ src/book/App.jsx
@@ -3,31 +3,25 @@ import React from "react"
 const App = () => {
   return (
     <section className="booklist">
-      <Book img={booklist[0].img} title={booklist[0].title} author={booklist[0].author} />
-      <Book img={booklist[1].img} title={booklist[1].title} author={booklist[1].author}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.</p>
-      </Book>
-      <Book img={booklist[2].img} title={booklist[2].title} author={booklist[2].author} />
+      {books.map((book) => {
+        return <Book book={book} />
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { img, alt, title, author, children } = props
+  const { img, alt, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} alt={alt} />
       <h1>{title}</h1>
       <h4>by {author}</h4>
-      {children}
     </article>
   )
 };
 
-const booklist = [
+const books = [
   {
     id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/5110qJqtLiL._SX403_BO1,204,203,200_.jpg",
